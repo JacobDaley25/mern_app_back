7bf8d3931cb6f7c5f5a447582925eb59249a934c
JacobDaley25 <96602300+JacobDaley25@users.noreply.github.com>
2022-03-04 12:34:46 -0500

    fixing typo

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const sessionsController = require('../controllers/sessions-controller.js');
 
-router.post('/', authController.handleLogin);
+router.post('/', sessionsController.handleLogin);
 
 module.exports = router;
